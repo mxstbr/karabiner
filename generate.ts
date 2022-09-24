0c446e69fdabc5b9c7b902c23801d52b2f464197
Max Stoiber <contact@mxstbr.com>
2022-09-24 18:00:09 +0200

    Add hyper key sublayer concept

--- generate.ts
+++ generate.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 import { KarabinerRules, Manipulator, To } from "./types";
 
+// JSON does not support Infinity, so this is to "fake" that
+const JSON_INFINITY_MS = 9999999999;
+
 /**
  * Custom way to describe a command in a layer
  */
@@ -39,6 +42,48 @@ function createHyperLayer(commands: { [key_code: string]: LayerCommand }) {
 }
 
 /**
+ * Create a Hyper Key sublayer, where every command is prefixed with a key
+ * e.g. Hyper + O ("Open") is the "open applications" layer, I can press
+ * e.g. Hyper + O + G ("Google Chrome") to open Chrome
+ */
+function createHyperSubLayer(
+  sublayer_key: string,
+  commands: { [key_code: string]: LayerCommand }
+): Manipulator[] {
+  return Object.keys(commands).map((command_key) => ({
+    ...commands[command_key],
+    type: "basic",
+    parameters: {
+      // By combining "infinity" simultanous threshold with key_down_order strict,
+      // I can press e.g. Hyper + O > G relatively slowly; since the Hyper key is
+      // unique anyway, this works great for my use cases to create hyper "sub layers"
+      "basic.simultaneous_threshold_milliseconds": JSON_INFINITY_MS,
+    },
+    from: {
+      simultaneous_options: {
+        key_down_order: "strict",
+      },
+      simultaneous: [
+        {
+          key_code: sublayer_key,
+        },
+        {
+          key_code: command_key,
+        },
+      ],
+      modifiers: {
+        mandatory: [
+          "left_command",
+          "left_control",
+          "left_shift",
+          "left_option",
+        ],
+      },
+    },
+  }));
+}
+
+/**
  * Open an app layer command
  */
 function app(name: string): LayerCommand {
@@ -53,7 +98,7 @@ function app(name: string): LayerCommand {
 }
 
 const rules: KarabinerRules[] = [
-  // Hyper key
+  // Define the Hyper key itself
   {
     description: "Hyper Key (⌃⌥⇧⌘)",
     manipulators: [
@@ -76,8 +121,19 @@ const rules: KarabinerRules[] = [
       },
     ],
   },
-
-  // Hyper layer
+  {
+    description: 'Hyper Key sublayer: O ("Open") ',
+    manipulators: createHyperSubLayer("o", {
+      g: app("Google Chrome"),
+      c: app("Cron"),
+      v: app("Visual Studio Code"),
+      d: app("Discord"),
+      s: app("Slack"),
+      e: app("Superhuman"),
+      n: app("Notion"),
+      t: app("Terminal"),
+    }),
+  },
   {
     description: "Hyper Key layer",
     manipulators: createHyperLayer({
@@ -99,14 +155,6 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
-      c: app("Cron"),
-      g: app("Google Chrome"),
-      v: app("Visual Studio Code"),
-      d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
-      n: app("Notion"),
-      t: app("Terminal"),
     }),
   },
 ];
--- types.ts
+++ types.ts
@@ -9,10 +9,25 @@ export interface Manipulator {
   from: From;
   to?: To[];
   to_if_alone?: To[];
+  parameters?: Parameters;
+}
+
+export interface Parameters {
+  "basic.simultaneous_threshold_milliseconds"?: number;
+}
+
+export interface SimultaneousFrom {
+  key_code: string;
+}
+
+export interface SimultaneousOptions {
+  key_down_order?: "insensitive" | "strict" | "strict_inverse";
 }
 
 export interface From {
-  key_code: string;
+  key_code?: string;
+  simultaneous?: SimultaneousFrom[];
+  simultaneous_options?: SimultaneousOptions;
   modifiers?: Modifiers;
 }
 
