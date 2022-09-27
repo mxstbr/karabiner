76791fac0e71b4e362e06c1cc0a19d2224aee21b
Max Stoiber <contact@mxstbr.com>
2022-09-27 10:48:28 +0200

    Revert "Experiment: direct hyper layer keys"
    This reverts commit 1f3ec157ab7da4432fdad639483945bd60b1bc2f.

--- types.ts
+++ types.ts
@@ -25,7 +25,6 @@ export interface SimultaneousFrom {
 
 export interface SimultaneousOptions {
   key_down_order?: "insensitive" | "strict" | "strict_inverse";
-  key_up_order?: "insensitive" | "strict" | "strict_inverse";
   detect_key_down_uninterruptedly?: boolean;
 }
 
