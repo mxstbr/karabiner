6f265c72e6f1110d1a361c6bd65e0c44ba4eb926
Max Stoiber <contact@mxstbr.com>
2023-04-26 17:14:28 -0700

    No longer need to disable CMD+Tab cause I'm so used to my quickswitch

--- rules.ts
+++ rules.ts
@@ -25,21 +25,21 @@ const rules: KarabinerRules[] = [
         ],
         type: "basic",
       },
-      {
-        type: "basic",
-        description: "Disable CMD + Tab to force Hyper Key usage",
-        from: {
-          key_code: "tab",
-          modifiers: {
-            mandatory: ["left_command"],
-          },
-        },
-        to: [
-          {
-            key_code: "tab",
-          },
-        ],
-      },
+//      {
+//        type: "basic",
+//        description: "Disable CMD + Tab to force Hyper Key usage",
+//        from: {
+//          key_code: "tab",
+//          modifiers: {
+//            mandatory: ["left_command"],
+//          },
+//        },
+//        to: [
+//          {
+//            key_code: "tab",
+//          },
+//        ],
+//      },
     ],
   },
   ...createHyperSubLayers({
