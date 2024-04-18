2d4b4b40c50d90d520b78c9bcd2e42db650d0ef6
Michael Muniz <mcmuniz218@gmail.com>
2024-04-18 13:44:11 -0400

    updates

--- rules.ts
+++ rules.ts
@@ -8,20 +8,30 @@ const rules: KarabinerRules[] = [
     description: "Hyper Key (⌃⌥⇧⌘)",
     manipulators: [
       {
-        description: "Caps Lock -> Hyper Key",
+        description: "Change caps_lock to command+control+option+shift.",
         from: {
-          key_code: "caps_lock",
-          modifiers: {
-            optional: ["any"],
-          },
+            key_code: "caps_lock",
+            modifiers: {
+                optional: [
+                    "any"
+                ]
+            }
         },
         to: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 1,
+            // {
+            //     key_code: "left_shift",
+            //     modifiers: [
+            //         "left_command",
+            //         "left_control",
+            //         "left_option"
+            //     ]
+            // },
+            {
+              set_variable: {
+                name: "hyper",
+                value: 1,
+              },
             },
-          },
         ],
         to_after_key_up: [
           {
@@ -36,8 +46,8 @@ const rules: KarabinerRules[] = [
             key_code: "escape",
           },
         ],
-        type: "basic",
-      },
+        type: "basic"
+    },
       //      {
       //        type: "basic",
       //        description: "Disable CMD + Tab to force Hyper Key usage",
@@ -56,122 +66,28 @@ const rules: KarabinerRules[] = [
     ],
   },
   ...createHyperSubLayers({
-    spacebar: open(
-      "raycast://extensions/stellate/mxstbr-commands/create-notion-todo"
-    ),
     // b = "B"rowse
     b: {
-      t: open("https://twitter.com"),
-      // Quarterly "P"lan
-      p: open("https://qrtr.ly/plan"),
-      y: open("https://news.ycombinator.com"),
-      f: open("https://facebook.com"),
-      r: open("https://reddit.com"),
+      y: open("https://youtube.com"),
+      c: open("https://crunchyroll.com"),
+      r: open("httpsL//rocketmoney.com"),
+      l: open("https://ldjam.com"),
+      g: open("https://github.com"),
     },
-    // o = "Open" applications
     o: {
-      1: app("1Password"),
-      g: app("Google Chrome"),
-      c: app("Notion Calendar"),
+      n: app("Obsidian"),
       v: app("Visual Studio Code"),
       d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
-      n: app("Notion"),
-      t: app("Warp"),
-      // Open todo list managed via *H*ypersonic
-      h: open(
-        "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
-      ),
-      z: app("zoom.us"),
-      // "M"essages
-      m: app("Texts"),
+      e: app("Spark Desktop"),
+      t: app("Todoist"),
       f: app("Finder"),
-      r: app("Texts"),
-      // "i"Message
-      i: app("Texts"),
-      p: app("Spotify"),
-      a: app("iA Presenter"),
-      // "W"hatsApp has been replaced by Texts
-      w: open("Texts"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
-      ),
-    },
-
-    // w = "Window" via rectangle.app
-    w: {
-      semicolon: {
-        description: "Window: Hide",
-        to: [
-          {
-            key_code: "h",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      y: rectangle("previous-display"),
-      o: rectangle("next-display"),
-      k: rectangle("top-half"),
-      j: rectangle("bottom-half"),
-      h: rectangle("left-half"),
-      l: rectangle("right-half"),
-      f: rectangle("maximize"),
-      u: {
-        description: "Window: Previous Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control", "right_shift"],
-          },
-        ],
-      },
-      i: {
-        description: "Window: Next Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control"],
-          },
-        ],
-      },
-      n: {
-        description: "Window: Next Window",
-        to: [
-          {
-            key_code: "grave_accent_and_tilde",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      b: {
-        description: "Window: Back",
-        to: [
-          {
-            key_code: "open_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      // Note: No literal connection. Both f and n are already taken.
-      m: {
-        description: "Window: Forward",
-        to: [
-          {
-            key_code: "close_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      d: {
-        description: "Window: Next display",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_control", "right_option", "right_command"],
-          },
-        ],
-      },
+      u: app("Unity Hub"),
+      m: app("Spotify"),
+      a: app("Arc"),
+      s: app("Slack"),
+      r: app("Rider 2024.1"),
+      // "G"ames
+      g: app("Steam"),
     },
 
     // s = "System"
@@ -226,6 +142,13 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
+      period: {
+        to: [
+          {
+            key_code: "rewind",
+          },
+        ],
+      },
       e: {
         to: [
           {
@@ -235,57 +158,8 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
-      // Turn on Elgato KeyLight
-      y: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 1, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
-      h: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 0, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
       // "D"o not disturb toggle
       d: open(`raycast://extensions/yakitrak/do-not-disturb/toggle`),
-    },
-
-    // v = "moVe" which isn't "m" because we want it to be on the left hand
-    // so that hjkl work like they do in vim
-    v: {
-      h: {
-        to: [{ key_code: "left_arrow" }],
-      },
-      j: {
-        to: [{ key_code: "down_arrow" }],
-      },
-      k: {
-        to: [{ key_code: "up_arrow" }],
-      },
-      l: {
-        to: [{ key_code: "right_arrow" }],
-      },
-      // Magicmove via homerow.app
-      m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
-      },
-      // Scroll mode via homerow.app
-      s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
-      },
-      d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
-      },
-      u: {
-        to: [{ key_code: "page_down" }],
-      },
-      i: {
-        to: [{ key_code: "page_up" }],
-      },
     },
 
     // c = Musi*c* which isn't "m" because we want it to be on the left hand
@@ -300,20 +174,24 @@ const rules: KarabinerRules[] = [
         to: [{ key_code: "rewind" }],
       },
     },
-
+    m: {
+      l: open("raycast://extensions/mattisssa/spotify-player/like"),
+      d: open("raycast://extensions/mattisssa/spotify-player/dislike"),
+      p: open("raycast://extensions/mattisssa/spotify-player/togglePlayPause"),
+      open_bracket: open("raycast://extensions/mattisssa/spotify-player/previous"),
+      close_bracket: open("raycast://extensions/mattisssa/spotify-player/next"),
+      s: open("raycast://extensions/mattisssa/spotify-player/toggleShuffle"),
+      r: open("raycast://extensions/mattisssa/spotify-player/cycleRepeat"),
+      w: open("raycast://extensions/mattisssa/spotify-player/devices"),
+      c: open("raycast://extensions/mattisssa/spotify-player/copyUrl"),
+    },
     // r = "Raycast"
     r: {
-      n: open("raycast://script-commands/dismiss-notifications"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-      ),
       e: open(
         "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
       ),
       c: open("raycast://extensions/raycast/system/open-camera"),
       p: open("raycast://extensions/raycast/raycast/confetti"),
-      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-      s: open("raycast://extensions/peduarte/silent-mention/index"),
       h: open(
         "raycast://extensions/raycast/clipboard-history/clipboard-history"
       ),
@@ -322,6 +200,12 @@ const rules: KarabinerRules[] = [
       ),
       2: open(
         "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
+      ),
+      3: open(
+        "raycast://extensions/VladCuciureanu/toothpick/disconnect-favorite-device-1"
+      ),
+      4: open(
+        "raycast://extensions/VladCuciureanu/toothpick/disconnect-favorite-device-2"
       ),
     },
   }),
