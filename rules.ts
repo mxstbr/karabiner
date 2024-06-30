8c6656efebd9abbcc7145eec06cc4568c1691ffa
Max Stoiber <contact@mxstbr.com>
2024-06-30 14:26:01 -0700

    Make Minecraft work across external split keyboard & laptop keyboard

--- rules.ts
+++ rules.ts
@@ -325,6 +325,30 @@ const rules: KarabinerRules[] = [
       ),
     },
   }),
+  {
+    description: "Change Backspace to Spacebar when Minecraft is focused",
+    manipulators: [
+      {
+        type: "basic",
+        from: {
+          key_code: "delete_or_backspace",
+        },
+        to: [
+          {
+            key_code: "spacebar",
+          },
+        ],
+        conditions: [
+          {
+            type: "frontmost_application_if",
+            file_paths: [
+              "^/Users/mxstbr/Library/Application Support/minecraft/runtime/java-runtime-gamma/mac-os-arm64/java-runtime-gamma/jre.bundle/Contents/Home/bin/java$",
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 fs.writeFileSync(
